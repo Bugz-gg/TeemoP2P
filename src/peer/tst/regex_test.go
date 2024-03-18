6845e29bc86449621aa889fa9dbe54e37d442480
kshao <kevin.shao@bordeaux-inp.fr>
2024-03-18 08:51:55 +0100

    GO Restructure to use the builtin "testing" package. Moved client.go and server.go to `main` directory.

--- src/peer/tst/regex_test.go
+++ src/peer/tst/regex_test.go
@@ -1,15 +1,19 @@
-package main
+package tools
 
 import (
 	"fmt"
-	tools "peerproject/tools"
+	"peerproject/tools"
+	"testing"
 )
 
-func main() {
+func TestInterested(t *testing.T) {
 	success, interestedData := tools.InterestedCheck("interested Uizhsja8hzUizhsja8hzUizhsja8hzsu")
 	fmt.Println(success, interestedData)
 	success2, interestedData2 := tools.InterestedCheck("interested izsja8hzUizhsja8hzUizhsja8hzsu")
 	fmt.Println(success2, interestedData2)
+}
+
+func TestHave(t *testing.T) {
 
 	success3, haveData := tools.HaveCheck("have Uizhsja8hzUizhsja8hzUizhsja8hzsu 010010101001")
 	fmt.Println(success3, haveData)
